0e26de1d00cd2e3dac580ee775925d34400c5e5a
COdinRyT <rthompso96@gmail.com>
2026-03-19 02:06:23 -0400

    Audio input recieved & possible text input

--- Speech Run/Assets/Scripts/AudioInput/CharacterController.cs
+++ Speech Run/Assets/Scripts/AudioInput/CharacterController.cs
@@ -10,24 +10,54 @@ public class CharacterController : MonoBehaviour
     [SerializeField]
     private Rigidbody characterPrefab;
 
+    private AudioClip audioClip;
+
     private Dictionary<string, Action> keywordActions = new Dictionary<string, Action>();
     private KeywordRecognizer keywordRecognizer;
+
+    [SerializeField] private bool isStop = true;
+    [SerializeField] private float characterSpeed = 1.0f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Start()
     {
+        MicrophoneAudio();
         keywordActions.Add("turn right", TurnRight);
         keywordActions.Add("turn left", TurnLeft);
+        keywordActions.Add("start", Forward);
+        keywordActions.Add("back", Back);
+        keywordActions.Add("stop", Stop);
 
         keywordRecognizer = new KeywordRecognizer(keywordActions.Keys.ToArray());
         keywordRecognizer.OnPhraseRecognized += OnKeywordRecognized;
         keywordRecognizer.Start();
     }
 
+    private void Update()
+    {
+        if (!isStop)
+        {
+            transform.position += Vector3.forward * characterSpeed * Time.deltaTime;
+        }
+        else
+        {
+            transform.position = Vector3.zero;
+        }
+    }
+
     private void OnKeywordRecognized(PhraseRecognizedEventArgs args)
     {
         Debug.Log("Keyword: " + args.text);
         keywordActions[args.text].Invoke();
+    }
+
+    public void MicrophoneAudio()
+    {
+        //Grabs the microphone device from input list
+        string microphoneName = Microphone.devices[0];
+
+        //Have the microphone start recording commands
+        audioClip = Microphone.Start(microphoneName, true, 20, AudioSettings.outputSampleRate);
     }
 
     //List of actions for the object
@@ -40,5 +70,20 @@ public class CharacterController : MonoBehaviour
     private void TurnRight()
     {
         transform.Rotate(0, 45, 0);
+    }
+
+    private void Forward()
+    {
+        isStop = false;
+    }
+
+    private void Back()
+    {
+        transform.position = Vector3.back;
+    }
+
+    private void Stop()
+    {
+        isStop = true;
     }
 }
